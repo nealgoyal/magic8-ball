8b46ca2471087599f8bee4bc70a1778fbc4ba11d
Neal Goyal <ngoya001@ucr.edu>
2018-02-01 05:44:46 -0800

    Updated UI/Added shake phone API

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballView: UIImageView!
+
+    var diceView : Int = 0
+    let randomBallNum = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        newBallImage()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    
+    func newBallImage() {
+        diceView = Int(arc4random_uniform(4))
+        ballView.image = UIImage(named: randomBallNum[diceView])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
 }
 
